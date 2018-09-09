ee21445ee0d400953ae60c07756c30430606c4ad
tpouhuk <ilovepotatoj@gmail.com>
2018-09-09 12:55:42 +0300

    added creating tables if not exist

--- database.py
+++ database.py
@@ -1,6 +1,31 @@
 import sqlite3
 from threading import Lock
 
+CREATE_DEALS = """
+CREATE TABLE `deals` (
+	`time`	INTEGER,
+	`sellerId`	TEXT,
+	`sellerCastle`	INTEGER,
+	`sellerName`	TEXT,
+	`buyerId`	TEXT,
+	`buyerCastle`	INTEGER,
+	`buyerName`	TEXT,
+	`item`	TEXT,
+	`qty`	INTEGER,
+	`price`	INTEGER
+);"""
+
+CREATE_OFFERS = """
+CREATE TABLE `offers` (
+	`time`	INTEGER,
+	`sellerId`	TEXT,
+	`sellerCastle`	INTEGER,
+	`sellerName`	TEXT,
+	`item`	TEXT,
+	`qty`	INTEGER,
+	`price`	INTEGER
+);"""
+
 def sync(locker):
     assert isinstance(locker, type(Lock())), 'ERROR: Lock class is required'
     def real_decorator(f):
@@ -33,5 +58,17 @@ def database_commit_close():
     database.commit()
     database.close()
 
+def check_and_create_tables():
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = [table[0] for table in c.fetchall()]
+
+    if 'deals' not in tables:
+        c.execute(CREATE_DEALS)
+    if 'offers' not in tables:
+        c.execute(CREATE_OFFERS)
+
+    database.commit()
+
 database = sqlite3.connect('cwapi.db', check_same_thread = False)
 c = database.cursor()
+check_and_create_tables()
